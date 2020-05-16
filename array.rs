6b02996e71c668a22378da76b0f0b79f17f524e0
Groot1895 <kponnivalavan95@gmail.com>
2020-05-16 20:50:45 +0530

    ...

--- array.rs
+++ array.rs
@@ -4,5 +4,10 @@ fn main() {
     //let a: [i32; 5] = [1, 2, 3, 4, 5]; // type of each element
     let a1 = [3; 5]; // all 5 elements are 3
     let first = a[0];// way of accessing ARRAY
+
     println!("{:?}\n{:?}\n{:?}\n{:?}\n",a,months,a1,first);
+
+    for i in &months {
+        println!("{}",i );
+    }
 }
